852a1cf13d5ede4f846e96d74e0a0b24f2633381
Yilin Liu <yliu469@wisc.edu>
2020-12-30 18:19:56 -0500

    Create BasicBlocks.py

---
+++ BasicBlocks.py
@@ -0,0 +1,183 @@
+from functools improt partial
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Conv2dAuto(nn.Conv2d):
+  def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.padding = (self.kernel_size[0] // 2, self.kernel_size[1] // 2)
+      
+conv3x3 = partial(Conv2dAuto, kernel_size=3, bias=False)
+
+def activation_func(activation):
+    return nn.ModuleDict([
+                ['relu', nn.ReLU(inplace=True)],
+                ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+                ['selu', nn.SELU(inplace=True)],
+                ['none', nn.Identity()]
+    ])
+
+def conv_bn(in_chn, out_chn, conv, *args, **kwargs):
+    
+    return nn.Sequential(conv(in_chn, out_chn, *args, **kwargs),
+           nn.BatchNorm2d(out_chn)
+    )
+
+
+class ResidualBlock(nn.Module):
+  def __init__(self, in_chns, out_chns, activation='relu'):
+      super().__init__()
+      self.in_chns, self.out_chns, self.activation = in_chns, out_chns, activation
+      self.blocks = nn.Identity()
+      self.activation = activation_func(activation)
+      self.shortcut = nn.Identity()
+      
+  def forward(self, x):
+      residual = x
+      if self.should_apply_shortcut: residual = self.shortcut(x)
+      x = self.blocks(x)
+      x += residual
+      x = self.activate(x)
+      return x
+      
+  @property
+  def should_apply_shortcut(self):
+      return self.in_chns != self.out_chns
+
+class ResNetResidualBlock(ResidualBlock):
+  def __init__(self, in_chns, out_chns, expansion=1, downsampling=1, conv=conv_bn, *args, **kwargs):
+      super().__init__(in_chns, out_chns, *args, **kwargs)
+      self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+      self.shortcut = nn.Sequential(
+          nn.Conv2d(self.in_chns, self.expanded_chns, kernel_size=1, 
+                    stride=self.downsampling, bias=False),
+          nn.BatchNorm2d(self.expanded_chns)) if self.should_apply_shortcut else None
+          
+  @property
+  def expanded_chns(self):
+      return self.out_chns * self.expansion
+      
+  @property
+  def should_apply_shortcut(self):
+      return self.in_chns != self.expansion_chns
+
+class ResidualBasicBlock(ResNetResidualBlock):
+  """
+    Basic ResNet block composed by two layers of 3x3conv/batchnorm/activation
+  """
+  expansion = 1
+  def __init__(self, in_chns, out_chns, *args, **kwargs):
+      super().__init__(in_chns, out_chns, *args, **kwargs)
+      self.blocks = nn.Sequential(
+          conv_bn(self.in_chns, self.out_chns, conv=self.conv, bias=False, stride=self.downsampling),
+          activation_func(self.activation),
+          conv_bn(self.out_chns, self.expanded_chns, conv=self.conv, bias=False)
+      )
+
+class ResNetBottleNeckBlock(ResNetResidualBlock):
+  expansion = 4
+  def __init__(self, in_chns, out_chns, *args, **kwargs):
+      super().__init__(in_chns, out_chns, expansion=4, *args, **kwargs)
+      self.blocks = nn.Sequential(
+        conv_bn(self.in_chns, self.out_chns, self.conv, kernel_size=1),
+        activation_func(self.activation),
+        conv_bn(self.out_chns, self.out_chns, self.conv, kernel_size=3, stride=self.downsampling),
+        activation_func(self.activation),
+        conv_bn(self.out_chns, self.expanded_chns, self.conv, kernel_size=1)
+      )
+      
+class ResNetLayer(nn.Module):
+      """
+      A ResNet layer composed by `n` blocks stacked one after the other
+      """
+      def __init__(self, in_chns, out_chns, block=ResNetBasicBlock, n=1, *args, **kwargs):
+          super().__init__()
+          downsampling = 2 if in_chns != out_chns else 1
+          self.blocks = nn.Sequential(
+              block(in_chns, out_chns, *args, **kwargs, downsampling=downsampling),
+              *[block(out_chns * block.expansion), 
+                    out_channels, downsampling=1, *args, **kwargs) for _ in range(n - 1)]
+          )
+          
+      def forward(self, x):
+          x = self.blocks(x)
+          return x
+   
+class ResNetEncoder(nn.Module):
+    """
+    ResNet encoder composed by layers with increasing features.
+    """
+    def __init__(self, in_chns=3, blocks_sizes=[64, 128, 256, 512], depths=[2,2,2,2], 
+                 activation='relu', block=ResNetBasicBlock, *args, **kwargs):
+        super().__init__()
+        self.blocks_sizes = blocks_sizes
+        
+        self.gate = nn.Sequential(
+            nn.Conv2d(in_chns, self.blocks_sizes[0], kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.blocks_sizes[0]),
+            activation_func(activation),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        
+        self.in_out_block_sizes = list(zip(blocks_sizes, blocks_sizes[1:]))
+        self.blocks = nn.ModuleList([ 
+            ResNetLayer(blocks_sizes[0], blocks_sizes[0], n=deepths[0], activation=activation, 
+                        block=block,*args, **kwargs),
+            *[ResNetLayer(in_chns * block.expansion, 
+                          out_chns, n=n, activation=activation, 
+                          block=block, *args, **kwargs) 
+              for (in_chns, out_chns), n in zip(self.in_out_block_sizes, depths[1:])]       
+        ])
+        
+        
+    def forward(self, x):
+        x = self.gate(x)
+        for block in self.blocks:
+            x = block(x)
+        return x
+        
+class ResnetDecoder(nn.Module):
+    """
+    This class represents the tail of ResNet. It performs a global pooling and maps the output to the
+    correct class by using a fully connected layer.
+    """
+    def __init__(self, in_features, n_classes):
+        super().__init__()
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
+        self.decoder = nn.Linear(in_features, n_classes)
+
+    def forward(self, x):
+        x = self.avg(x)
+        x = x.view(x.size(0), -1)
+        x = self.decoder(x)
+        return x
+        
+class ResNet(nn.Module):
+    
+    def __init__(self, in_channels, n_classes, *args, **kwargs):
+        super().__init__()
+        self.encoder = ResNetEncoder(in_channels, *args, **kwargs)
+        self.decoder = ResnetDecoder(self.encoder.blocks[-1].blocks[-1].expanded_channels, n_classes)
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+def resnet18(in_channels, n_classes, block=ResNetBasicBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[2, 2, 2, 2], *args, **kwargs)
+
+def resnet34(in_channels, n_classes, block=ResNetBasicBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 6, 3], *args, **kwargs)
+
+def resnet50(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 6, 3], *args, **kwargs)
+
+def resnet101(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 23, 3], *args, **kwargs)
+
+def resnet152(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 8, 36, 3], *args, **kwargs)
+      
+      
+      
